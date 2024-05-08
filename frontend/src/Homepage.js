a10763c967259cdfe7a274880288a7da31876d36
pnaruka <pankajnaruka111@gmail.com>
2024-05-08 12:49:14 +0530

    product cards added

--- frontend/src/Homepage.js
+++ frontend/src/Homepage.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux';
+import { getProducts } from './contexts_store/reducer/products';
+import { useFetchProducts } from './hooks/useFetchProducts';
+import { SimpleGrid } from '@chakra-ui/react';
+import ProductCard from './components/productCard';
 
 const Homepage = () => {
-  return (
-    <div>Homepage</div>
-  )
+    const products = useSelector(getProducts);
+    const { fetchProducts } = useFetchProducts();
+
+    useEffect(() => {
+        fetchProducts();
+    }, [])
+    return (
+        <div>
+            <SimpleGrid spacing="40px" templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
+                {products.map(product => (
+                    <ProductCard key={product.id} product={product} />
+                ))}
+            </SimpleGrid>
+        </div>
+    )
 }
 
 export default Homepage
